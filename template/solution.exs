aa0963937970825be58b707f0e819c99777bd960
Alex Pearce <alex@alexpearce.me>
2022-12-12 15:14:30 +0000

    Add input loading to template.

--- template/solution.exs
+++ template/solution.exs
@@ -4,6 +4,11 @@ defmodule Solution do
 
   def part2() do
   end
+
+  defp input() do
+    File.stream!("00/input.txt")
+      |> Enum.map(&String.trim/1)
+  end
 end
 
 IO.puts "Part 1: #{Solution.part1}"
